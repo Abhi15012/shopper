2133b79577aad9b2b328bf7da9ad5ce3d02583b8
pavan peddy <pavan.06.p@gmail.com>
2024-03-11 18:41:34 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ command }) => {
+  const isProduction = command === "build --mode production";
+
+  return defineConfig({
+    plugins: [
+      react(),
+      checker({
+        eslint: {
+          lintCommand: 'eslint "./src//*.{js,jsx,ts,tsx}"',
+        },
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: /^~(.+)/,
+          replacement: path.join(process.cwd(), "node_modules/$1"),
+        },
+        {
+          find: /^src(.+)/,
+          replacement: path.join(process.cwd(), "src/$1"),
+        },
+      ],
+    },
+    server: {
+      port: 3030,
+    },
+    build: {
+      outDir: "dist",
+      minify: isProduction,
+      sourcemap: !isProduction,
+    },
+  });
+};
